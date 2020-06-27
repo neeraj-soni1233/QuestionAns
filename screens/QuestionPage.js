5ab8ead3bdab6f232bca26519c7a29af0d6bfd6e
Neha <neha@Nehas-MacBook-Pro.local>
2020-06-27 23:32:39 +0530

    QuestionList box

--- screens/QuestionPage.js
+++ screens/QuestionPage.js
@@ -1,44 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TouchableOpacity, Alert } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
+import Constants from 'expo-constants';
 
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
+function Item({ id, title, selected, onSelect }) {
+    return (
+        <TouchableOpacity
+            onPress={() => onSelect(id)}
+            style={[
+                styles.item,
+                { backgroundColor: selected ? '#008000' : '#99CCFF' },
+            ]}
+        >
+            <Text>{title}</Text>
 
+        </TouchableOpacity>
+    );
+}
 
 export default function QuestionPage(props) {
     const [text, setText] = React.useState('Quiz');
     const [userName, setUserName] = React.useState('Neeraj');
-    const [question, setQuestion] = React.useState(' Q1: Hi What is React native');
+    const [question, setQuestion] = React.useState(' Q1: Hi What React native');
+    const [selected, setSelected] = React.useState(new Map());
+
+    const onSelect = React.useCallback(
+        id => {
+            const newSelected = new Map(selected);
+            newSelected.set(id, !selected.get(id));
+
+            setSelected(newSelected);
+        },
+        [selected],
+    );
     return (
         <View style={styles.container}>
-
-            {/* <View style={styles.topLabel}>
-                <Button  width={150} mode= 'outlined' mode="text" >
-                    Quiz
-                 </Button>
-
-                <Button borderColor={'#008000'} width={150} mode='outlined' mode="text" >
-                    Neeraj
-                 </Button>
-            </View> */}
             <View style={styles.Label}>
-                <TextInput  width = {100}disabled={true}
-                    label= {text} mode="outlined"
+                <TextInput width={100} disabled={true}
+                    label={text} mode="outlined"
                 />
-                <TextInput width = {100} disabled={true}
-                    label= {userName} mode="outlined"
+                <TextInput width={100} disabled={true}
+                    label={userName} mode="outlined"
                 />
 
             </View>
 
-            <View style = {styles.QuestionText} >
-                <TextInput disabled={true} 
-                   mode="flat"
+            <View style={styles.QuestionText} >
+                <TextInput disabled={true}
+                    mode="flat"
                     value={question}
+                    multiline = {true}
                     onChangeText={question => setQuestion(question)}
                 />
-
             </View>
+
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={DATA}
+                    renderItem={({ item }) => (
+                        <Item
+                            id={item.id}
+                            title={item.title}
+                            selected={!!selected.get(item.id)}
+                            onSelect={onSelect}
+                        />
+                    )}
+                    keyExtractor={item => item.id}
+                    extraData={selected}
+                />
+            </SafeAreaView>
+
+            <View style={styles.button}>
+                <Button color = "#008000" width={150} mode= 'contained'  >
+                    Back
+                 </Button>
+
+                <Button color = "#008000"  width={150} mode='contained' >
+                    Next
+                 </Button>
+            </View>
+
+
         </View>
 
     );
@@ -72,13 +129,31 @@ const styles = StyleSheet.create({
     QuestionText: {
         backgroundColor: '#fff',
         padding: 20,
-        
+
     },
-     theme : {
-        color :'#A9A9A9',
-        backgroundColor: "transparent" 
-        }
-      
-      
+
+    listContainer: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+
+    },
+    item: {
+        backgroundColor: '#99CCFF',
+        padding: 15,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+    button: {
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        padding:20,
+         
+     },
+
 
 });
+
+
